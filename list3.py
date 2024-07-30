9500b5f76b57cea15ed8a7adbff14bd885c47395
Jenifer <jeniferjenifer980@example.com>
2024-07-30 08:22:13 +0530

    list changes

---
+++ list3.py
@@ -0,0 +1,10 @@
+#List is a Ordered data type.
+#List supports INDEXING and SLICING.
+#List allows DUPLICATE.
+#List is a MUTABLE data type.
+#Default value of list is Empty Square Brackets.
+
+
+a='python'
+x=a.upper()
+print(x)
