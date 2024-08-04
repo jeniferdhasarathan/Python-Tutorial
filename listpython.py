64de3e80bd6b62963f1b1bef1087807ee71b8994
Jenifer <jeniferjenifer980@example.com>
2024-08-04 18:34:44 +0530

    tuple

--- listpython.py
+++ listpython.py
@@ -249,5 +249,59 @@
 #o/p=[1,2,[3,4]]
 
 
+#CONCATENATION:
+#It is used to join two or more string ,list,tuple,set,dictionary.
+
+#EX:
+#s='hello'
+#h='hi'
+#a=s+h
+#a='hello'+'hi'
+#o/p='hellohi'
+
+#REPLICATION:
+#It is used to duplicate the given collection to a specific number of times.
+
+#EX:
+#10*20
+#200
+
+#TUPLE:
+#It is used to store both homogeneous and heterogeneous type of data.
+#Boundaries of tuple is () paranthesis.
+#Tuple is a ordered data type.
+#Tuple sopports indexing and slicing.
+#Tuple is a immutable data type.
+#Default value of tuple is empty () paranthesis.
+
+#ex:s=(1,2,3,4,5)
+#type(s)
+#<class 'tuple'>
+
+#s=(1,2,3,4,5)
+#type error:Tuple expected at most one argument got 5.
+
+#TUPLE METHODS:
+#It is used to find the position of a first occured specified element.
+#It returns integer.
+
+#SYNTAX:
+#variable.index(element,[si,ei])
+
+#Ex:
+#s=(1,2,3,4,5)
+#s.index(2)
+#o/p=1
+
+#COUNT():
+#It is used to return how many times the specified element is present.
+
+#SYNTAX:
+#variablename.count(character)
+
+
+#s=(1,2,3,4,5,2,1,4,3,2,1,4)
+#s.count(2)
+3
  
 
