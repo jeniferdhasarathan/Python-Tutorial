fb24c253e7680a3a473e0161bb52c1d0cb7a60d4
Jenifer <jeniferjenifer980@example.com>
2024-08-05 18:56:36 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,38 @@
+#TUPLE:
+#It is used to store both homogeneous and heterogeneous type of data.
+#Boundaries of tuple is () paranthesis.
+#Tuple is a ordered data type.
+#Tuple sopports indexing and slicing.
+#Tuple is a immutable data type.
+#Default value of tuple is empty () paranthesis.
+
+#ex:s=(1,2,3,4,5)
+#type(s)
+#<class 'tuple'>
+
+#s=(1,2,3,4,5)
+#type error:Tuple expected at most one argument got 5.
+
+#TUPLE METHODS:
+#It is used to find the position of a first occured specified element.
+#It returns integer.
+
+#SYNTAX:
+#variable.index(element,[si,ei])
+
+#Ex:
+#s=(1,2,3,4,5)
+#s.index(2)
+#o/p=1
+
+#COUNT():
+#It is used to return how many times the specified element is present.
+
+#SYNTAX:
+#variablename.count(character)
+
+
+#s=(1,2,3,4,5,2,1,4,3,2,1,4)
+#s.count(2)
+3
+ 
