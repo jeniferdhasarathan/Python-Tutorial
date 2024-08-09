e34d37d7ece6ad4564db4c832b35f0aeace87acf
Jenifer <jeniferjenifer980@example.com>
2024-08-09 21:37:30 +0530

    python methods

--- pythonintro.py
+++ pythonintro.py
@@ -160,6 +160,173 @@
 #syntax:
 # COLLECTION_NAME(START INDEX:END INDEX:STEP VALUE)
 
-#
+#Here,start index is optional,if we are start from 0.
+# End index is optional,if we ends at length.
+# Step value is optional,if we want to extract characters after every step.
+# 
+# NOTE: IN SLICING, END INDEX IS EXCLUSIVE,WON'T BE CONSIDER.
+# 
+# ex:  a='morning'
+# print([0:2:1])
+# o/p= 
 
 
+#STRING METHODS:
+#1.  UPPER()
+#2.  LOWER()
+#3.  SWAPCASE()
+#4.  CAPITALIZE()
+#5.  TITLE()
+#6.  INDEX()
+#7.  rINDEX()
+#8.  FIND()
+#9.  rFIND()
+#10.  COUNT()
+#11.  REPLACE()
+#12. SPLIT()
+#13. rSPLIT()
+#14. JOIN()
+#15. STRIP()
+#16. rSTRIP()
+#17. lSTRIP()
+#18. STARTSWITH()
+#19. ENDSWITH()
+#20. ISUPPER()
+#21. ISLOWER()
+#22. ISALPHA()
+#23. ISDIGIT()
+#24. ISALNUM()
+#25. ISSPACE()
+#26. ISIDENTIFIER()
+
+#1.UPPER():
+#It is a string method ,which accepts string as an input and convert that string into uppercase characters.
+#Return type of upper is String.
+#It won't accept any arguments.
+
+#SYNTAX:
+# string.upper()
+
+#ex:  a='hello'
+#s.upper()
+#o/p='HELLO'
+
+#LOWER():
+#It is a string method, which accept string as an input and convert the string into lowercase characters.
+#It returns string.
+#It won't accept any arguments.
+
+#SYNTAX:
+#string.lower()
+
+#ex: a='JESUS'
+#a.lower()
+#o/p= 'jesus'
+
+#SWAPCASE():
+#It is a string method ,which accept string as an input and convert all the uppercase character into lowercase and lowercase character into uppercase.
+#It returns string.
+#It won't accept any arguments.
+
+#SYNTAX:
+#string.swapcase()
+
+#ex: c='JesUS'
+#c.swapcase()
+#o/p='jESus'
+
+#CAPITALIZE():
+#It is a string method, which converts first letter of a string into uppercase and remaining letters into lowercase.
+#It returns string.
+#It won't accepts any arguments.
+
+#SYNTAX:
+#string.capitalize()
+
+#ex: a='love is blind'
+#a.capitalize()
+#o/p:'Love is blind'
+
+#TITLE():
+#It is a string method ,which accepts string and return the string by converting first letter of every word into uppercase and remaining lwtter into lowercase.
+# It returns string.
+#It won't accept any arguments.
+
+# SYNTAX:
+#  string.title()
+# 
+# ex: a='hello everyone here'
+# a.title()
+#o/p='Hello Everyone Here'
+#  
+
+#INDEX():
+#It is a string method, is used to find first occured specified element from the left side.
+# The return type of index is 'INTEGER'.
+# It accepts 3 arguments.
+# 1.element or substring.
+# 2.start index
+# 3.end index
+# 
+# By default start index is 0 and end index is length of the collection.
+# It  rises VALUE ERROR,if the specified element is not present in the collection.
+# 
+# SYNTAX:
+# string.index(substring,[SI,EI]).........[] indicates the optional.
+# 
+# ex:
+# a='jenifer'
+# a.index('e',0,5)
+#o/p=1
+# 
+# 
+# rINDEX():
+# It is a string method,which is used to find the position of a first occured specified element from the right side.
+# It returns 'INTEGER'.
+
+# It accepts 3 arguments.
+# 1.element or substring.
+# 2.start index
+# 3.end index  
+
+#SYNTAX:
+#string.Rindex(substring,[SI,EI]).........[] indicates the optional.
+
+#EX: a='jenifer'
+#a.rindex('e')
+#o/p=2
+
+
+#FIND()
+
+# It is a string method,which is used to find the position of a first occured specified element from the LEFT side.
+#  It returns 'INTEGER'.
+# It accepts 3 arguments.
+# 1.element or substring.
+# 2.start index
+# 3.end index  
+
+##SYNTAX:
+#string.FIND(substring,[SI,EI]).........[] indicates the optional.
+
+
+#ex: a='jenifer'
+#a.find('n')
+#o/p=2
+
+
+#rFIND():
+
+#It is a string method,which is used to find the position of a first occured specified element from the RIGHT side.
+#  It returns 'INTEGER'.
+# It accepts 3 arguments.
+# 1.element or substring.
+# 2.start index
+# 3.end index  
+
+##SYNTAX:
+#string.rFIND(substring,[SI,EI]).........[] indicates the optional.
+
+#ex: a='APPLE'
+#a.Rfind('P')
+#o/p=
