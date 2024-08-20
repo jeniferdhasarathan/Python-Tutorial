9f1abf5d00b2b60a42a0585f1f9fda4fd2d38bef
Jenifer <jeniferjenifer980@example.com>
2024-08-20 20:52:45 +0530

    dictionary

---
+++ dic.py
@@ -0,0 +1,21 @@
+#DICTIONARY:
+
+#Dictionary is a collection of key and value pair.
+#keys and values are separated by colon.
+#keys and value pairs are separated by comma(,).
+#it is an ordered data type.
+#it does not support indexing and slicing.
+#Duplicate keys are allowed but duplicate values are not allowed.
+#Keys must be immutable data type.But values can be any python data type.
+#Dictionary   is a mutable data type.
+#Boundaries of dictionary is curly bracket{}.
+# Default value of dictionary is empty curly bracket{}.
+# To access the value ,we need to use keys.
+# 
+# SYNTAX:
+#        {key:value,key:value} 
+
+#ex:
+#dic={'a':1,'b':2,'c':3}
+#dic['a']
+#o/p=> 1
