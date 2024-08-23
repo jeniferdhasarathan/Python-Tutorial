cd6f04d778acc2cb388b9131f63af126e8143eac
Jenifer <jeniferjenifer980@example.com>
2024-08-23 20:26:51 +0530

    set changes

--- set.py
+++ set.py
@@ -122,3 +122,175 @@
 #s1.symmetric_difference_update('hello2')
 #s1
 #o/p={'2','hello'}
+
+#TO ADD AN ELEMENT INTO THE SET:
+#1.ADD()
+#2.UPDATE()
+
+#1.ADD():
+#Add() will add the element randomly.
+#It accepts on;y immutable data type.
+#It throws type error, if any mutable data type are passed.
+
+#syntax:
+#set.add(element)
+
+#ex:
+#s={11,22,33,44}
+#s.add(55)
+#o/p=> {11,22,33,44,55}
+
+#UPDATE():
+#It si used to add the element random to a set.
+#It will unpack the element and dd to a set.
+#It accepts iterable.
+
+#ex:
+#s={1,2,3,4}
+#s.update('hello')
+#o/p=> {1,2,3,4,'h','e','l','l','o'}
+
+
+#TO REMOVE AN ELEMENTS FROM THE SET:
+#1.remove()
+#2.Discard()
+#3.pop()
+#4.clear()
+
+#1.REMOVE():
+#It is used to remove the specified element from the set.
+#It rises key error,if the specified element is not present in the set.
+
+#SYNTAX:
+#set.remove(element)
+
+#ex:
+#s={1,2,3,'hello',20}
+#print(s)
+#{1,2,3,'hello',20}
+
+#DISCARD():
+#It is used to remove the specified element from the set.
+#It won't throws any error,if the specified element is not present in a specified set.
+
+#SYNTAX:
+#set.discard(element)
+
+#ex:
+##s={1,2,3,4,5,6}
+#s.discard(2)
+#o/p=> {1,3,4,5,6}
+
+
+#POP():
+#it is used to remove the specified element from a set.
+#it won't accept any arguments.
+#it rises error if the set is empty.
+
+#SYNTAX:
+#set.pop()
+
+#ex:
+#s={1,2,3,4,5,6}
+#set.pop()
+#o/p=> 1
+
+#CLEAR():
+
+#It is used to remove all the elements from a set.
+#It won't accept any arguments.
+
+#syntax:
+#set.clear()
+
+#ex:
+#s={1,2,3,4,5,6}
+#s.clear()
+
+
+#IS SUPERSET():
+
+#It returns true,if all the values of iterable are present in base set.
+#Else it returns false.
+
+#syntax:
+#base set.issuperset(iterable)
+
+#ex:
+#s={1,2,3,4,5,6}
+#s.issuperset(s1)
+#o/p=> True
+
+#IS SUBSET():
+
+#it returns true,if all the values of base set are present in a iterable.
+#else it returns false.
+
+#SYNtax:
+#base set.issubset(iterable)
+
+#ex:
+#s={1,2,3}
+#s1={2,3,4,5,6,7,1}
+#s.issubset(s1)
+#True
+
+#IS DISJOINT():
+#it returna true ,if base set and iterable contains different elements.
+
+#SYNtax:
+#base set.isdisjoint()
+
+#ex:
+#s={1,2,3,4}
+#s1={5,6,7,8}
+#s.isdisjoint(s1)
+#True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
