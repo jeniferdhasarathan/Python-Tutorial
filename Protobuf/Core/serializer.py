4b235f1d812de0fb9b0b3a038821c92573e3e018
Arunkumar Thangarj <you@example.com>
2024-08-23 21:51:05 +0530

    write a api

--- Protobuf/Core/serializer.py
+++ Protobuf/Core/serializer.py
@@ -6,3 +6,46 @@ class SampleModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = SampleModel
         fields = ['id', 'name', 'description', 'data']
+
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Project
+
+User = get_user_model()
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'description', 'owner', 'is_default']
+
+class UserSerializer(serializers.ModelSerializer):
+    default_project_name = serializers.CharField(write_only=True, required=True)
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'is_admin', 'default_project_name']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        default_project_name = validated_data.pop('default_project_name')
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        # Create default project for this user
+        Project.objects.create(
+            name=default_project_name,
+            owner=user,
+            is_default=True
+        )
+
+        return user
+
+
+class AdminSerializer(UserSerializer):
+    
+    def create(self, validated_data):
+        validated_data['is_admin'] = True
+        return super().create(validated_data)
--- Protobuf/Core/urls.py
+++ Protobuf/Core/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path,include
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, AdminViewSet, ProjectViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'admins', AdminViewSet, basename='admin')
+router.register(r'projects', ProjectViewSet, basename='project')
 
 urlpatterns = [
-    path('sample/',views.SampleDataAPIView.as_view()),
+    path('', include(router.urls)),
 ]
