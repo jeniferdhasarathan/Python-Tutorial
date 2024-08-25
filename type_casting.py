2e3dbd566c917adcc39d9c83c8baa93470d0e0dd
Jenifer <jeniferjenifer980@example.com>
2024-08-25 20:35:28 +0530

    typecasting

---
+++ type_casting.py
@@ -0,0 +1,72 @@
+#TYPE CASTING:
+# The process of converting one data type into another data type is called type casting.
+
+#1.INTEGER:
+# We can convert integer into:
+#       float
+#       boolean
+#       complex
+#       string
+
+
+#2.FLOAT:
+#We can convert float into:
+#      integer
+#      boolean
+#      complex
+#      string 
+#
+
+#3.BOOLEAN:
+# We can convert boolean into:
+#      integer
+#      float
+#      complex
+#      string
+
+
+#4.COMPLEX:
+# We can convert complex into:
+#      boolean
+#      string
+
+
+#5.STRING:
+# We can convert string into:
+#      integer  (only one digit and integer value)
+#      float    (only digit)
+#      boolean  
+#      complex  (one one string)
+#      list
+#      tuple
+#      set
+
+#6.LIST:
+# We can convert list into:
+#      boolean
+#      string
+#      tuple
+
+
+#7.TUPLE:
+# We can convert tuple into:
+#      boolean
+#      string
+#      list
+#      set
+#      dictionary  (if tuple of values are present inside a tuple)
+
+#8.SET:
+# We can convert set into:
+#      boolean
+#      string
+#      list
+#      tuple
+#
+
+#9.Dictionary:
+# We can convert dictionary into:
+#      boolean
+#      string
+#
+#For list,tuple,set- By defaultly keys will typecast.
